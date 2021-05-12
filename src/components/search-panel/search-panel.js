8dbe99cc03dfddf007e584c538e3dc93b3419544
Vanes <vanes.sysoev@gmail.com>
2021-05-12 17:26:13 +0300

    feat: search-panel
    made search from input

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './search-panel.css';
 
-const SearchPanel = () => {
-    return (
-        <input
-            className="form-control search-input"
-            type="text"
-            placeholder="Поиск по записям"
-        />
-    )
-}
+export default class SearchPanel extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.onUpdeateSearch = this.onUpdeateSearch.bind(this);
+    }
 
-export default SearchPanel;
+    onUpdeateSearch(event){
+        const term = event.target.value;
+        this.setState({term});
+        this.props.onUpdeateSearch(term);
+    }
+
+    render(){
+        return (
+            <input
+                className="form-control search-input"
+                type="text"
+                placeholder="Поиск по записям"
+                onChange={this.onUpdeateSearch}
+            />
+        )
+    }
+}
